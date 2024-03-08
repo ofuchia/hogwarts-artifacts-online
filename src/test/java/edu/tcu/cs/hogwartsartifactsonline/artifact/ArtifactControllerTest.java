573d3d0434b5a3f83022c41a56f029efdf185ab0
ofuchia <o.akpom@tcu.edu>
2024-03-07 23:33:54 -0600

    Saving. Start of video 21

---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactControllerTest.java
@@ -0,0 +1,24 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtifactControllerTest {
+
+    ArtifactService artifactService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findArtifactById() {
+    }
+}
