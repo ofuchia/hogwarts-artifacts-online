f882b45af4be29b6a55d80aef5d82347a7dd49ef
ofuchia <o.akpom@tcu.edu>
2024-04-28 00:47:42 -0500

    Connect to Azure Databse for MySQL with Azure Key Vault

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/DBDataInitializer.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/DBDataInitializer.java
@@ -7,9 +7,11 @@ import edu.tcu.cs.hogwartsartifactsonline.hogwartsuser.UserService;
 import edu.tcu.cs.hogwartsartifactsonline.wizard.Wizard;
 import edu.tcu.cs.hogwartsartifactsonline.wizard.WizardRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component //now a bean of SB like Controller, Serice, and Repository
+@Profile("dev")
 public class DBDataInitializer implements CommandLineRunner {
 
     private final ArtifactRepository artifactRepository;
